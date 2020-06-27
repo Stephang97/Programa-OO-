8223d234c991a8a57b86862c3b416ea97f425174
Abraham <eas.gavilanez@yavirac.edu.ec>
2020-06-26 23:39:14 -0500

    primer comit

---
+++ Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Programa
+{
+    
+        class Usuario
+        {
+
+            public string Nombre { get; set; }
+            public int Id { get; set; }
+            public string Edad { get; set; }
+            public string Direccion { get; set; }
+
+        }
+
+    
+}
